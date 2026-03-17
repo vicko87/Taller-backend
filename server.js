01bdc56d68ab63a035418eb58d6bf8382cfc2f9c
Victoria <vkolomytseva87@gmail.com>
2026-03-17 18:35:19 +0100

    correccion de server.js

--- server.js
+++ server.js
@@ -1,4 +1,9 @@
-
+import express from 'express'
+import cors from 'cors'
+import dotenv from 'dotenv'
+import mongoose from 'mongoose'
+import reservationsRouter from './routes/reservations'
+import contactRouter from './routes/contact'
 
 
 
